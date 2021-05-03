580c57c22b9b5596ea931ccf736282d1326d6e07
Prathiba-H-02 <83569654+Prathiba-H-02@users.noreply.github.com>
2021-05-03 12:00:00 +0530

    Add files via upload
    Ultrasonic sensor

---
+++ ultrasonic.h
@@ -0,0 +1,13 @@
+#define ULTRA_H
+#include"Arduino.h"
+
+class ULTRASONIC
+{
+  public:  
+    int trig=0;
+    int echo=0;
+    int dist=0;
+    void begin(int trigger_pin, int echo_pin);
+    int ultra(void);  
+};
+#endif
